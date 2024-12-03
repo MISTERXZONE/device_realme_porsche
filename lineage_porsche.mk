956231c46ee2f9c2b6fae6bb4d232e357356b6a4
MISTERXZONE <porschemisterx@gmail.com>
2024-12-03 21:01:39 +0530

    RisingOS 15 Bringup

--- lineage_porsche.mk
+++ lineage_porsche.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/porsche/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_porsche
+PRODUCT_NAME := lineage_porsche
 PRODUCT_DEVICE := porsche
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -22,6 +22,26 @@ PRODUCT_MODEL := RMX3312
 
 PRODUCT_SYSTEM_NAME := RE58B2L1
 PRODUCT_SYSTEM_DEVICE := RE58B2L1
+
+#Rising Stuff
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+TARGET_USES_OPLUS_CAMERA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_UDFPS_ANIMATIONS := true 
+EXTRA_UDFPS_ANIMATIONS := true
+
+# GMS and Launcher
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# Maintainer
+RISING_MAINTAINER=MISTER_X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
